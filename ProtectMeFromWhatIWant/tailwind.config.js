a18dbc1988286df717e7a67279d8b22924af492d
hfg-fabienne-v <fabienne.vatter@hfg.design>
2024-01-17 16:41:00 +0100

    install fonts and added navbar

--- ProtectMeFromWhatIWant/tailwind.config.js
+++ ProtectMeFromWhatIWant/tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily :{ 
+        PxGrotesk: ["PxGrotesk","screen"],
+        poppins: ['Poppins', 'sans-serif'],
+      } 
+    },
   },
   plugins: [],
 };
