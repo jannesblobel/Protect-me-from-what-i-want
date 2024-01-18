a3c8990026441407d86ee8dbb5b80337d20c7f91
hfg-fabienne-v <fabienne.vatter@hfg.design>
2024-01-18 15:00:36 +0100

    digital reflection edited

--- ProtectMeFromWhatIWant/src/components/Navbar.tsx
+++ ProtectMeFromWhatIWant/src/components/Navbar.tsx
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom";
 import logo from "../assets/logo.png";
 import "./styles/Navbar.css";
 // import DigitalReflection from '../pages/digital-reflection';
@@ -9,18 +10,18 @@ export default function Navbar() {
         <img src={logo} className="logo" />
       </a>
       <div className="nav-tabs">
-        <a href="/digital-reflection" className="tab">
-          DIGITAL REFLECTION
-        </a>
-        <a href="/media-competence" className="tab">
-          MEDIA COMPETENCE
-        </a>
-        <a href="/act-now" className="tab">
-          ACT NOW
-        </a>
-        <a href="/who-else" className="tab">
-          WHO ELSE?
-        </a>
+        <NavLink to="/digital-reflection" className={({ isActive }) => (isActive ? "tab active" : "tab")}>
+          digital reflection
+        </NavLink>
+        <NavLink to="/media-competence" className={({ isActive }) => (isActive ? "tab active" : "tab")}>
+            media competence
+        </NavLink>
+        <NavLink to="/act-now" className={({ isActive }) => (isActive ? "tab active" : "tab")}>
+            act now
+        </NavLink>
+        <NavLink to="/who-else" className={({ isActive }) => (isActive ? "tab active" : "tab")}>
+            who else?
+        </NavLink>
       </div>
     </nav>
   );
