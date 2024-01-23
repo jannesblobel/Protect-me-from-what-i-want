79d5b24c995aa5be3b0dd0b8aff01b0a82018c04
Joschua Rothenbacher <filzbold@gmail.com>
2024-01-23 16:19:41 +0100

    updated SpotLightScene.tsx

--- ProtectMeFromWhatIWant/src/components/SpotLightScene.tsx
+++ ProtectMeFromWhatIWant/src/components/SpotLightScene.tsx
@@ -1,9 +1,19 @@
 import { Plane, SpotLight } from "@react-three/drei";
-import { Canvas } from "@react-three/fiber";
-import { useState } from "react";
+import { Canvas, useFrame, useThree } from "@react-three/fiber";
+import { useEffect, useRef, useState } from "react";
 import * as THREE from "three";
 import "./styles/SpotLight.css";
 
+// Function to map one range of numbers to another range
+const mapRange = (
+  value: number,
+  inMin: number,
+  inMax: number,
+  outMin: number,
+  outMax: number
+) => {
+  return ((value - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
+};
 type ControlledSpotLightProps = {
   intensity: number;
   angle: number;
@@ -13,8 +23,25 @@ type ControlledSpotLightProps = {
 
 function ControlledSpotLight(props: ControlledSpotLightProps) {
   const { intensity, angle, penumbra, distance } = props;
+  const spotLightRef = useRef<THREE.SpotLight>(null);
+  const viewport = useThree((state) => state.viewport);
+  const vec = new THREE.Vector3();
+  useFrame((state) => {
+    if (!spotLightRef.current) return;
+    spotLightRef.current.target.position.lerp(
+      vec.set(
+        (state.mouse.x * viewport.width) / 2,
+        (state.mouse.y * viewport.height) / 2,
+        0
+      ),
+      0.1
+    );
+    spotLightRef.current.target.updateMatrixWorld();
+  });
+
   return (
     <SpotLight
+      ref={spotLightRef}
       intensity={intensity}
       angle={angle}
       penumbra={penumbra}
@@ -63,16 +90,17 @@ export default function SpotLightScene() {
   const [intensity, setIntensity] = useState(50);
   const [angle, setAngle] = useState(Math.PI / 8);
   const [penumbra, setPenumbra] = useState(0.1);
-  const [distance, setDistance] = useState(8);
+  const [distance, setDistance] = useState(7);
 
   const cameraPosition = new THREE.Vector3(5, 5, 5);
-  const cameraZoom = 1; // Increase or decrease this value to adjust the size of the view
+  const cameraZoom = 90; // Increase or decrease this value to adjust the size of the view
 
   return (
     <div className="spotlight-container">
       <div className="canvas-container">
         <Canvas
           shadows
+          orthographic
           camera={{
             position: cameraPosition,
             zoom: cameraZoom,
@@ -81,6 +109,12 @@ export default function SpotLightScene() {
           }}
         >
           <ambientLight intensity={0.5} />
+          <pointLight
+            position={[0, 3.5, 0]}
+            intensity={10}
+            distance={10}
+            decay={2}
+          />
           <ControlledSpotLight
             intensity={intensity}
             angle={angle}
@@ -93,29 +127,29 @@ export default function SpotLightScene() {
       {/* Sliders rendered outside of Canvas */}
       <div>
         <div>
-          <label>Intensity: </label>
+          <label>Fokus: </label>
           <input
             type="range"
-            min="10"
-            max="300"
-            step="10"
-            value={intensity}
-            onChange={(e) => setIntensity(Number(e.target.value))}
-          />
-        </div>
-        <div>
-          <label>Angle: </label>
-          <input
-            type="range"
-            min="0.01"
-            max={Math.PI / 2}
+            min="0.10"
+            max={Math.PI / 2 - 0.1}
             step="0.01"
-            value={angle}
-            onChange={(e) => setAngle(Number(e.target.value))}
+            value={Math.PI / 2 - angle}
+            onChange={(e) => {
+              setAngle(Math.PI / 2 - Number(e.target.value));
+              setIntensity(
+                mapRange(
+                  Number(e.target.value),
+                  0.1,
+                  Math.PI / 2 - 0.1,
+                  30,
+                  500
+                )
+              );
+            }}
           />
         </div>
         <div>
-          <label>Penumbra: </label>
+          <label>Streulicht: </label>
           <input
             type="range"
             min="0"
@@ -126,12 +160,12 @@ export default function SpotLightScene() {
           />
         </div>
         <div>
-          <label>Distance: </label>
+          <label>Aufmerksamkeit: </label>
           <input
             type="range"
             min="6"
-            max="20"
-            step="0.5"
+            max="7"
+            step="0.1"
             value={distance}
             onChange={(e) => setDistance(Number(e.target.value))}
           />
